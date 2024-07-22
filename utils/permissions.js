b0cec26696f44d9a80ea6a393b2babf37e6d0931
Pranjali Dhale <pranjalidhale@Pranjalis-MacBook-Pro.local>
2024-07-22 23:01:07 +0530

    Changes updated

---
+++ utils/permissions.js
@@ -0,0 +1,4 @@
+exports.PERMISSIONS = ["createschema1", "updateschema1", "deleteschema1", "readschema1"]; 
+exports.ROLES = ["manager"] 
+exports.MANAGER = ["create", "update"]; 
+exports.RESTRICTED_ROLES = ["intern"]; 
